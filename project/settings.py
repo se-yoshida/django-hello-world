79a0cc4dde8a034b2cdf8810e56d1568530714e7
YoshidaSatoshi <se.yoshida.satoshi@gmail.com>
2020-06-04 21:22:36 +0900

    Djangoの初期設定

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'r2&e_qc3gv+ho&iibt&@ei83o)yggt19nugewulo*&clsj9pyh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']   # 全てのドメインからの接続を許可
 
 
 # Application definition
@@ -103,9 +104,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'    # 言語コードを日本語に設定
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'    # タイムゾーンを日本語に設定
 
 USE_I18N = True
 
